7bc02549ff17b651a9486a99bc77c42db6273104
Chandler <watson@facni.com>
2019-06-10 01:49:01 -0700

    Add beneath logic

--- src/bot.py
+++ src/bot.py
@@ -22,11 +22,20 @@ class RandomBot:
 
 
 class ModelBasedBot:
+    PATTERNS = [string.ascii_letters, '+', '>', '$', '-', '|']
+
     def __init__(self):
         self.prev_state = None
         self.prev_act = None
+        self.prev_map = None
+        self.beneath = None
         self.observations = []
         self.exploring = True
+        # state_size = 
+        # self.transition_probs = [[[1.0 / len(MOVE_ACTIONS)
+        #                            for _ in range(len(
+        self.transition_probs = None
+        self.rewards = None
 
     def find_self(self, state_map):
         for y in range(len(state_map)):
@@ -35,37 +44,53 @@ class ModelBasedBot:
                     return x, y
         return None
 
-    def compute_probs(self):
-        pass
-
     def get_neighbors(self, state_map, x, y):
         neighbors = []
         for dx in (-1, 0, 1):
             for dy in (-1, 0, 1):
                 if dx == dy == 0:
                     continue
-                neighbors.append(state_map[y][x])
+                neighbors.append(state_map[y + dy][x + dx])
         return neighbors
 
+    def update_prev_map(self, new_map):
+        replaced_map = self.prev_map
+        self.prev_map = new_map
+        for line, row in enumerate(self.prev_map):
+            if '@' in row:
+                if replaced_map is None:
+                    beneath = '.'
+                else:
+                    beneath = replaced_map[line][row.index('@')]
+                self.prev_map[line] = row.replace('@', beneath)
+                break
+
     def parse_state(self, state):
-        parsed = []
         pair = self.find_self(state['map'])
-        if pair is None:
-            return None
-        x, y = pair
-        neighbors = self.get_neighbors(state['map'], x, y)
-        for pattern in (string.ascii_letters, '+', '>', '$', '-|'):
-            for neighbor in neighbors:
-                parsed.append(neighbor in pattern)
+        if pair is None or self.prev_map is None:
+            parsed = None
+        else:
+            parsed = []
+            x, y = pair
+            self.beneath = self.prev_map[y][x]
+            neighbors = self.get_neighbors(state['map'], x, y)
+            for pattern in self.PATTERNS:
+                for neighbor in neighbors:
+                    parsed.append(neighbor in pattern)
+                parsed.append(self.beneath in pattern)
+        self.update_prev_map(state['map'])
         return parsed
 
     def choose_action(self, state):
+        if not self.exploring and self.epoch > self.prob_update_epoch:
+            self.update_probs()
         if state['message']['is_more']:
             act = action.Action.MORE
         elif state['message']['is_yn']:
             act = action.Action.YES
         else:
             parsed_state = self.parse_state(state)
+            # TODO: unify by initializing probs
             if self.exploring:
                 if self.prev_state is not None:
                     observation = (self.prev_state, self.prev_act,
@@ -78,6 +103,9 @@ class ModelBasedBot:
             self.prev_act = act
         return act
 
+    def update_probs_and_rewards():
+        pass
+
     def get_status(self):
         exp_string = 'LORE' if self.exploring else 'LOIT'
         train_string = 'TRAIN' if self.train else 'TEST'
@@ -86,7 +114,11 @@ class ModelBasedBot:
                 and self.observations[-1][-1] is not None):
             binary_rep = ''.join(['1' if part else '0' for part in
                                   self.observations[-1][-1]])
-            status += '\tST:{:05x}'.format(int(binary_rep, 2))
+            status += '\tST:{:012x}'.format(int(binary_rep, 2))
+        if self.beneath is not None:
+            status += '\tBN:{}'.format(self.beneath)
+        else:
+            status += '\tHELP'
         if self.prev_act is not None:
             status += '\t{}'.format(self.prev_act)
         return status
