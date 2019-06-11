54e1ee8922a461abb902f270ca403c5a9595a923
Chandler <watson@facni.com>
2019-06-11 13:00:13 -0700

    Make run_q.py

--- src/run_q.py
+++ src/run_q.py
@@ -5,6 +5,7 @@ import experiment
 
 exp_game = game.Game()
 exp_display = display.Display()
-exp_bot = bot.QLearningBot(discount=0.2)
-exp = experiment.Experiment(exp_bot, exp_game, exp_display)
-exp.run(show=True, epochs=50)
+for discount in (0.2, 0.4, 0.6, 0.8):
+    exp_bot = bot.QLearningBot(discount=discount)
+    exp = experiment.Experiment(exp_bot, exp_game, exp_display)
+    exp.run(verbose=True, epochs=20)
